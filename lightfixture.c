88928e34975656cc84abc89d7c53a98c6f7fa3e3
Cody Claborn <cxsquared@gmail.com>
2015-04-29 19:35:01 -0400

    Learning how to create max externals and at the same time kinda learning C. Things are getting started though.

--- lightfixture.c
+++ lightfixture.c
@@ -12,6 +12,12 @@
 typedef struct _lightfixture
 {
     t_object ob;			// the object itself (must be first)
+    void *outlet1;
+    void *outlet2;
+    long size;
+    long* output;
+    long address;
+    long colorIndex;
 } t_lightfixture;
 
 ///////////////////////// function prototypes
@@ -19,6 +25,9 @@ typedef struct _lightfixture
 void *lightfixture_new(t_symbol *s, long argc, t_atom *argv);
 void lightfixture_free(t_lightfixture *x);
 void lightfixture_assist(t_lightfixture *x, void *b, long m, long a, char *s);
+
+void lightfixture_bang(t_lightfixture *x);
+void lightfixture_message(t_lightfixture *x, t_symbol *s, long argc, t_atom *argv);
 
 //////////////////////// global class pointer variable
 void *lightfixture_class;
@@ -29,6 +38,8 @@ int C74_EXPORT main(void)
     t_class *c;
     
     c = class_new("lightfixture", (method)lightfixture_new, (method)lightfixture_free, (long)sizeof(t_lightfixture), 0L /* leave NULL!! */, A_GIMME, 0);
+    
+    class_addmethod(c, (method)lightfixture_bang, "bang", 0);
     
     /* you CAN'T call this from the patcher */
     class_addmethod(c, (method)lightfixture_assist,	"assist", A_CANT, 0);
@@ -52,25 +63,9 @@ void lightfixture_assist(t_lightfixture *x, void *b, long m, long a, char *s)
 
 void lightfixture_free(t_lightfixture *x)
 {
-    ;
+    free(x->output);
 }
 
-/*
- A_GIMME signature =
-	t_symbol	*s		objectname
-	long		argc	num additonal args
-	t_atom		*argv	array of t_atom structs
- type = argv->a_type
- if (type == A_LONG) ;
- else if (type == A_FLOAT) ;
- else if (type == A_SYM) ;
- */
-/*
-	t_symbol {
- char *s_name;
- t_object *s_thing;
-	}
- */
 void *lightfixture_new(t_symbol *s, long argc, t_atom *argv)
 {
     t_lightfixture *x = NULL;
@@ -78,20 +73,45 @@ void *lightfixture_new(t_symbol *s, long argc, t_atom *argv)
     
     // object instantiation
     if (x = (t_lightfixture *)object_alloc(lightfixture_class)) {
+        if (argc != 2) {
+            return NULL;
+        }
+        
         object_post((t_object *)x, "a new %s object was instantiated: %p", s->s_name, x);
         object_post((t_object *)x, "it has %ld arguments", argc);
         
+        // Checking for arguments
         for (i = 0; i < argc; i++) {
             if ((argv + i)->a_type == A_LONG) {
+                if (i == 0){
+                    x->address = atom_getlong(argv+i);
+                } else if (i == 1){
+                    x->size = atom_getlong(argv+1);
+                }
                 object_post((t_object *)x, "arg %ld: long (%ld)", i, atom_getlong(argv+i));
-            } else if ((argv + i)->a_type == A_FLOAT) {
-                object_post((t_object *)x, "arg %ld: float (%f)", i, atom_getfloat(argv+i));
-            } else if ((argv + i)->a_type == A_SYM) {
-                object_post((t_object *)x, "arg %ld: symbol (%s)", i, atom_getsym(argv+i)->s_name);
             } else {
-                object_error((t_object *)x, "forbidden argument");
+                return NULL;
             }
         }
+        
+        x->output = (long*)malloc(sizeof(long) * x->size);
+        for (i=0; i < x->size; i++) {
+            x->output[i] = 0;
+        }
+        x->outlet2 = outlet_new((t_object *) x, NULL);
+        x->outlet1 = outlet_new((t_object *) x, NULL);
     }
     return (x);
+}
+
+void lightfixture_bang(t_lightfixture *x) {
+    t_atom myList[x->size];
+    short i;
+
+    for (i=0; i < x->size; i++) {
+        //object_post((t_object *) x, "output &ld is &ld", i, x->output[i]);
+        atom_setlong(myList+i,x->output[i]);
+    }
+    outlet_list(x->outlet1,0L,x->size,&myList);
+    outlet_int(x->outlet2, x->size);
 }
